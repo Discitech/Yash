db5a9b36873d632eeac5ce0937965716c214c5e7
Discitech <69648998+Discitech@users.noreply.github.com>
2020-09-03 17:37:56 +0530

    Add files via upload

---
+++ auth-routes.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+
+//auth login
+router.get('/login',(req,res,next) => {
+	res.send('this time fail')
+});
+
+//auth logout
+router.get('/logout',(req,res,next) => {
+	res.send('logging out');
+	//req.logout(;);
+	//res.redirect('/');
+});
+
+//create redirect
+router.get('/google/redirect',passport.authenticate('google'),(req,res,next) => {
+	res.send('redirect the thing to the page');
+});
+
+//auth with google
+router.get('/google',passport.authenticate('google',{
+	scope:['profile','email']
+}));
+
+router.get('/auth/facebook/redirect',
+  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  (req, res) => {
+    // Successful authentication, redirect home.
+    res.send('here we have to redirect')
+  });
+ 
+router.get('/facebook',passport.authenticate('facebook',{
+	scope:['profile']
+}));
+
+module.exports = router;
